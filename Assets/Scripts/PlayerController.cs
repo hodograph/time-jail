7b2c2ddf8f478e53105cab0a4f78ae5f3e0360ef
Austin James Medina <30534842+medinajaustin@users.noreply.github.com>
2017-10-19 11:25:23 -0400

    First Commit - Empty Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speedMultiplier;
+
+	// Use this for initialization
+	void Start () {
+
+		speedMultiplier = 1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveVert = Input.GetAxis ("Vertical");
+		float moveHoriz = Input.GetAxis ("Horizontal");
+
+		Vector3 Move = new Vector3 (moveHoriz, 0.0f, moveVert);
+
+		transform.Translate(Move * speedMultiplier);
+		
+	}
+}

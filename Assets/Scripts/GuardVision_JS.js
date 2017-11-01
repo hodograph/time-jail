4a8505b5bff35b7b8474e2971624859775385084
Jeremy <jjdeanman@gmail.com>
2017-11-01 12:34:06 -0400

    no message

---
+++ Assets/Scripts/GuardVision_JS.js
@@ -0,0 +1,39 @@
+﻿var playerObject : GameObject; // the player
+var fieldOfViewRange : float; // in degrees (I use 68, this gives the enemy a vision of 136 degrees)
+var minPlayerDetectDistance : float; // the distance the player can come behind the enemy without being deteacted
+var rayRange : float; // distance the enemy can "see" in front of him
+private var rayDirection = Vector3.zero;
+
+function CanSeePlayer() : boolean
+{
+     var hit : RaycastHit;
+     var rayDirection = playerObject.transform.position - transform.position;
+     var distanceToPlayer = Vector3.Distance(transform.position, playerObject.transform.position);
+ 
+     if(Physics.Raycast (transform.position, rayDirection, hit))
+	{ // If the player is very close behind the player and in view the enemy will detect the player
+         if((hit.transform.tag == "Player") && (distanceToPlayer <= minPlayerDetectDistance))
+         {
+         	return true;
+         }
+	}
+
+	 if((Vector3.Angle(rayDirection, transform.forward)) < fieldOfViewRange)
+	 { // Detect if player is within the field of view
+         if (Physics.Raycast (transform.position, rayDirection, hit))
+          {
+ 
+             if (hit.transform.tag == "Player") 
+             {
+                 //Debug.Log("Can see player");
+                 return true;
+             }
+             else
+             {
+                 //Debug.Log("Can not see player");
+                 return false;
+             }
+         }
+     }
+ }
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,73 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speedMultiplier;
+	public Rigidbody rb;
+	Matrix4x4 baseMatrix = Matrix4x4.identity;
+
+	// Used to calibrate the Input.acceleration
+	void CalibrateAccelerometer()
+	{
+		/*Currently only works in quadrants 2&4
+		 * 
+		 *                  UP
+		 *                  ^
+		                    |
+		             1      |       2
+		                    |
+		                    |
+		Back <--------------+------------- -> Person/Screen facing
+		                    |
+		                    |
+		             4      |      3
+		                    |
+		                    V
+		                   Down
+		  Not sure how or why this works.
+		*/
+		Vector3 Accel = Input.acceleration;
+		Accel.z *= -1;
+		Quaternion rotate = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), Accel);
+		Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, rotate, new Vector3(1.0f, 1.0f, 1.0f));
+		this.baseMatrix = matrix.inverse;
+	}
 
 	// Use this for initialization
 	void Start () {
 
-		speedMultiplier = 1;
+		speedMultiplier = 20f;
+		rb = GetComponent<Rigidbody>();
+		CalibrateAccelerometer ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		Vector3 fixedAcceleration = this.baseMatrix.MultiplyVector (Input.acceleration);
+		//Debug.Log ("Fixed: " + fixedAcceleration);
+		//Debug.Log ("Original: " + Input.acceleration);
+		rb.velocity = fixedAcceleration * speedMultiplier;
 
 		float moveVert = Input.GetAxis ("Vertical");
 		float moveHoriz = Input.GetAxis ("Horizontal");
+		if (moveVert != 0.0f || moveHoriz != 0.0f) {
 
-		Vector3 Move = new Vector3 (moveHoriz, 0.0f, moveVert);
+			Vector3 Move = new Vector3 (moveHoriz, 0.0f, moveVert);
 
-		transform.Translate(Move * speedMultiplier);
-		
+			rb.velocity = Move * speedMultiplier;
+		}
+	}
+
+	void OnGUI(){
+		//Calibrate button
+		if (GUI.Button(new Rect(Screen.width/10*9, Screen.height/20*18, Screen.width/10, Screen.width/20), "Calibrate")) {
+			CalibrateAccelerometer ();
+			Debug.Log ("Calibrating");
+		}
+
+		//Sprint button (repeat does on hold)
+		if(GUI.RepeatButton(new Rect(Screen.width/10*9, Screen.height/20*16, Screen.width/10, Screen.width/20), "Sprint")){
+			speedMultiplier = 80f;
+		}else{
+			speedMultiplier = 20f;
+		}
 	}
 }
